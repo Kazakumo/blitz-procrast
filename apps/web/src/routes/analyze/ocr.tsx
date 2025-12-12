6bfdf60f6911dde8a144158976ec9955da4d17f1
Kazakumo <shunya.sato1402@gmail.com>
2025-12-13 01:10:14 +0900

    feat: 画像アップロード・プレビュー表示機能追加

--- apps/web/src/routes/analyze/ocr.tsx
+++ apps/web/src/routes/analyze/ocr.tsx
@@ -1,9 +1,42 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
-export const Route = createFileRoute("/analyze/ocr") ({
-    component: RouteComponent
-})
+export const Route = createFileRoute("/analyze/ocr")({
+	component: RouteComponent,
+});
 
-function RouteComponent () {
-    return <div>hoge</div>
+function RouteComponent() {
+	// TODO: 画像をアップロードできるようにして、ボタンを押すとocrかける
+	// ocrをかけたデータを表示する
+	const [image, setImage] = useState<File>();
+	const [url, setUrl] = useState<string>("");
+	const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (
+			e.target.files !== undefined &&
+			e.target.files !== null &&
+			e.target.files?.length !== 0
+		) {
+			setImage(e.target.files[0]);
+			const url = URL.createObjectURL(e.target.files[0]);
+			setUrl(url);
+		}
+	};
+	async function handleClick() {
+		console.log(url);
+		//TODO: ocrする
+	}
+
+	return (
+		<>
+			<input
+				type="file"
+				accept="image/*"
+				onChange={(e) => handleImageChange(e)}
+			/>
+			<img src={url} alt="" />
+			<button type="button" onClick={() => handleClick()}>
+				OCR
+			</button>
+		</>
+	);
 }
